e7e223878026e78f52bf0501ecfec8e3bd0c6a9a
AdnanAShaikh <shaikhadnan862@gmail.com>
2024-08-05 06:26:31 +0530

    deploy-2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // routes
 // user routes
@@ -29,14 +29,6 @@ const blogRoutes = require("./routes/blogRoutes");
 app.use("/api/v1/blog", blogRoutes);
 
 // Catch-all handler to serve the React app
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
 
 const PORT = process.env.PORT || 3715;
 
